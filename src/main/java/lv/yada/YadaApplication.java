4ce667a5e56f655a40e19af1e5c88bd43f98f261
retriku <renars.kudins@gmail.com>
2018-04-13 10:24:00 +0300

    3: try gradle with spring boot
    - added deps
    
    Task-Url: https://github.com/BonuxSolutions/yada/issues/issues/3

---
+++ src/main/java/lv/yada/YadaApplication.java
@@ -0,0 +1,12 @@
+package lv.yada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YadaApplication.class, args);
+	}
+}
