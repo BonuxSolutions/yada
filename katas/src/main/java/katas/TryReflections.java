cd6aa0a31ce82fd1bce2902f0f5e69e48e890a01
Renārs Kudiņš <renars.kudins@gmail.com>
2018-05-15 21:46:23 +0300

    katas

---
+++ katas/src/main/java/katas/TryReflections.java
@@ -0,0 +1,43 @@
+package katas;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@A
+class C {
+    private String a;
+}
+
+public class TryReflections {
+
+    private Object c;
+
+    public static void main(String[] a) {
+        var cl = TryReflections.class.getClassLoader();
+        var packages = Arrays
+                .stream(cl.getDefinedPackages())
+                .filter(p -> "katas".equals(p.getName()))
+                .findFirst().get();
+        try {
+            var files = Files.list(Paths.get(cl.getResource(packages.getName()).toURI()))
+                    .map(f -> packages.getName() + "/" + f.getFileName().toString());
+            var classes = files.map(cl::getResource).map(r -> {
+                try {
+                    var t = r.toURI().relativize(cl.getResource(".").toURI()).toString();
+                    return r.toURI().toString().substring(t.length());
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            classes.forEach(System.out::println);
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+}
