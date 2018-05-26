1de8d4c80ca1aca768a36047c0d9fcb0a8188d8a
Renārs Kudiņš <renars.kudins@gmail.com>
2018-05-26 13:11:44 +0300

    yada-7: write unit/integration tests - test spring context and user details

--- app/src/test/java/bonux/yada/YadaSpringContextIntegrationTest.java
+++ app/src/test/java/bonux/yada/YadaSpringContextIntegrationTest.java
@@ -1,16 +1,40 @@
 package bonux.yada;
 
+import bonux.yada.auth.Roles;
+import bonux.yada.auth.YadaUserDetailsService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = {YadaUserDetailsService.class})
 @DataJpaTest
 public class YadaSpringContextIntegrationTest {
 
-    @Test
-    public void testSetup() {
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
+    @Test
+    public void testSpringContext() {
+        assertNotNull(applicationContext);
+    }
+
+    @Test
+    public void testUserDetailsService() {
+        var user = userDetailsService.loadUserByUsername("admin");
+        var authorities = new ArrayList<>(user.getAuthorities());
+        assertEquals("admin", user.getUsername());
+        assertTrue(authorities.stream().anyMatch(p -> p.getAuthority().equals(Roles.ADMIN.toString())));
     }
 }
--- app/src/test/java/module-info.java
+++ app/src/test/java/module-info.java
@@ -1,5 +1,17 @@
 module bonux.yada {
+    requires bonux.yada.auth;
+    requires bonux.yada.todo;
+
+    uses bonux.yada.auth.YadaSecurityConfig;
+    uses bonux.yada.auth.YadaUserDetailsService;
+
+    requires spring.beans;
+    requires spring.context;
     requires spring.test;
+    requires spring.security.core;
+    requires spring.security.config;
+    requires spring.boot.autoconfigure;
     requires spring.boot.test.autoconfigure;
+    requires spring.boot.test;
     requires junit;
 }
